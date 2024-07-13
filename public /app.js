d05fb5ab2420332392415044d2fdb789a1dac2ca
Adesopequizzify <muizadesope83@gmail.com>
2024-07-13 15:50:15 +0100

    update

---
+++ public /app.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArdahTZ6qxbBRSPf_BzC6ECjrdEQzUwY8",
+  authDomain: "zephyr-io.firebaseapp.com",
+  databaseURL: "https://zephyr-io-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "zephyr-io",
+  storageBucket: "zephyr-io.appspot.com",
+  messagingSenderId: "138997878507",
+  appId: "1:138997878507:web:2771777eabb49262e3f8ae",
+  measurementId: "G-GBLY5EHF1X"
+};
+
+module.exports = firebaseConfig; // Export only the firebaseConfig
